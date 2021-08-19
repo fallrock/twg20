571e3a976197ff972ce3a88c224fac718712d7ef
Dmitry <deekovalev@gmail.com>
2021-08-19 17:48:56 +0300

    this. for members

--- Assets/Player/CharacterMovement.cs
+++ Assets/Player/CharacterMovement.cs
@@ -11,9 +11,11 @@ public class CharacterMovement : MonoBehaviour
     public float steeringMinSpeed = 3f;
     public float steerAcceleration = 15f;
     public float jumpImpulse = 5f;
-    [Range(0f, 1f)] public float steerSmoothing = 0.95f;
+    [Range(0f, 1f)]
+    public float steerSmoothing = 0.95f;
     public float afkDeceleration = 1f;
-    [Range(0f, 1f)] public float friction = 0.005f;
+    [Range(0f, 1f)]
+    public float friction = 0.005f;
     public Transform directionProvider;
 
     private int onGround = 0;
@@ -22,17 +24,17 @@ public class CharacterMovement : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-        onGround++;
+        this.onGround++;
     }
 
     private void OnTriggerExit(Collider other) {
-        onGround--;
+        this.onGround--;
     }
 
     private void Update() {
-        if (onGround != 0 && Input.GetKey(KeyCode.Space)) {
+        if (this.onGround != 0 && Input.GetKey(KeyCode.Space)) {
             Vector3 velocity = GetComponent<Rigidbody>().velocity;
-            velocity.y = jumpImpulse;
+            velocity.y = this.jumpImpulse;
             GetComponent<Rigidbody>().velocity = velocity;
         }
     }
@@ -41,11 +43,11 @@ public class CharacterMovement : MonoBehaviour
         Vector3 f = GetWorldSpaceInput();
         if (f != Vector3.zero) {
             float speed = Vector3.ProjectOnPlane(GetComponent<Rigidbody>().velocity, Vector3.up).magnitude;
-            float t = Mathf.Clamp01(Mathf.InverseLerp(steeringMinSpeed, rawAccelerationMaxSpeed, speed));
+            float t = Mathf.Clamp01(Mathf.InverseLerp(this.steeringMinSpeed, this.rawAccelerationMaxSpeed, speed));
             ApplyLowSpeedAcceleration(f, 1f - t);
             ApplySteering(f, t);
         }
-        if (onGround != 0) {
+        if (this.onGround != 0) {
             ApplyFriction();
             if (f == Vector3.zero) {
                 ApplyAfkDeceleration();
@@ -59,8 +61,8 @@ public class CharacterMovement : MonoBehaviour
         if (input == Vector3.zero) { return Vector3.zero; }
         input.Normalize();
 
-        Vector3 forward = Vector3.ProjectOnPlane(directionProvider.forward, Vector3.up).normalized;
-        Vector3 right = directionProvider.right;
+        Vector3 forward = Vector3.ProjectOnPlane(this.directionProvider.forward, Vector3.up).normalized;
+        Vector3 right = this.directionProvider.right;
         Vector3 forceDirection = input.z * forward + input.x * right;
         return forceDirection;
     }
@@ -83,7 +85,7 @@ public class CharacterMovement : MonoBehaviour
         }
         Vector3 currentDirection = currentVelocity.normalized;
         float t = Mathf.Clamp01(-Vector3.Dot(wantedDirection, currentDirection));
-        return Mathf.Lerp(acceleration, deceleration * Mathf.Sign(acceleration), t);
+        return Mathf.Lerp(acceleration, this.deceleration * Mathf.Sign(acceleration), t);
     }
 
     private void ApplyLowSpeedAcceleration(Vector3 forceDirection, float scale) {
@@ -92,7 +94,7 @@ public class CharacterMovement : MonoBehaviour
 
         float currentSpeed = currentVelocity.magnitude;
 
-        float acceleration = baseRawAcceleration * scale;
+        float acceleration = this.baseRawAcceleration * scale;
         acceleration = MixDeceleration(acceleration, forceDirection);
         rb.AddForce(forceDirection * acceleration, ForceMode.Acceleration);
 
@@ -110,7 +112,7 @@ public class CharacterMovement : MonoBehaviour
         if (currentDirection != Vector3.zero) {
             currentDirection.Normalize();
         }
-        Vector3 smoothWantedDirection = Vector3.Slerp(wantedDirection, currentDirection, steerSmoothing);
+        Vector3 smoothWantedDirection = Vector3.Slerp(wantedDirection, currentDirection, this.steerSmoothing);
 
         var currentRotation = Quaternion.LookRotation(currentDirection);
         var wantedRotation = Quaternion.LookRotation(smoothWantedDirection);
@@ -118,16 +120,16 @@ public class CharacterMovement : MonoBehaviour
         float wantedAngle = Quaternion.Angle(currentRotation, wantedRotation);
         float currentSpeed = currentVelocity.magnitude;
 
-        float allowedAngle = steerAcceleration / currentSpeed;  // Physically accurate
+        float allowedAngle = this.steerAcceleration / currentSpeed;  // Physically accurate
         allowedAngle = Mathf.Clamp(allowedAngle, 0f, 90f);  // For scale to work
         allowedAngle *= Mathf.Pow(Mathf.Clamp01(Vector3.Dot(wantedDirection, currentDirection) + 1f), 0.5f);
         allowedAngle *= scale;
         float t = Mathf.Clamp01(wantedAngle != 0f ? allowedAngle / wantedAngle : 1f);
 
         rb.velocity = Vector3.Slerp(currentVelocity, smoothWantedDirection * currentSpeed, t);
-        float acceleration = Vector3.Dot(wantedDirection, currentDirection) * steeringForwardAcceleration;
+        float acceleration = Vector3.Dot(wantedDirection, currentDirection) * this.steeringForwardAcceleration;
         acceleration = MixDeceleration(acceleration, wantedDirection);
-        if (onGround != 0 || Vector3.Dot(wantedDirection, currentDirection) < 0f) {
+        if (this.onGround != 0 || Vector3.Dot(wantedDirection, currentDirection) < 0f) {
             rb.AddForce(currentDirection * acceleration * scale, ForceMode.Acceleration);
         }
         rb.velocity += savedY * Vector3.up;
@@ -137,15 +139,15 @@ public class CharacterMovement : MonoBehaviour
 
     private void ApplyFriction() {
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(-rb.velocity * friction, ForceMode.VelocityChange);
+        rb.AddForce(-rb.velocity * this.friction, ForceMode.VelocityChange);
     }
 
     private void ApplyAfkDeceleration() {
         Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb.velocity.magnitude <= afkDeceleration * Time.fixedDeltaTime) {
+        if (rb.velocity.magnitude <= this.afkDeceleration * Time.fixedDeltaTime) {
             rb.velocity = Vector3.zero;
         }
-        rb.AddForce(-rb.velocity.normalized * afkDeceleration, ForceMode.Acceleration);
+        rb.AddForce(-rb.velocity.normalized * this.afkDeceleration, ForceMode.Acceleration);
     }
 
 }
