bda1961917be7fce4781e2c7c7871a55ca523222
grillow <spmr757@gmail.com>
2021-08-20 17:29:47 +0300

    done refactoring

--- Assets/Cloning/Clone/CloneBehaviour.cs
+++ Assets/Cloning/Clone/CloneBehaviour.cs
@@ -6,14 +6,14 @@ public class CloneBehaviour : MonoBehaviour
 {
     public GameObject explosionPrefab;
 
+    void Start()
+    {
+        GetComponent<TrajectoryReproducer>().StartReproducing();
+    }
+
     void FixedUpdate()
     {
-        if (trajectory.Count == 0) return;
-        float localRoundTime = Time.time - currentRoundBeginning;
-        var currentPosition =
-            trajectory.FindLast(x => x.Item1 < localRoundTime);
-        transform.position = currentPosition.Item2;
-        if (currentPosition == trajectory[trajectory.Count - 1]) {
+        if (GetComponent<TrajectoryReproducer>().finished) {
             Explode();
         }
     }
@@ -25,18 +25,5 @@ public class CloneBehaviour : MonoBehaviour
         );
         GameObject.Destroy(gameObject);
     }
-
-    public void StartRound(float time) {
-        currentRoundBeginning = Time.time;
-    }
-    public void StartRound() {
-        StartRound(Time.time);
-    }
-
-    // [HideInInspector]
-    private float currentRoundBeginning;
-
-    public List<(float, Vector3)> trajectory
-      = new List<(float, Vector3)>();
 
 }
