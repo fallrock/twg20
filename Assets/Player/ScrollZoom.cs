24f8c28582e0006d6b90e78a42e4ebdd6aa8734b
Dmitry <deekovalev@gmail.com>
2021-08-23 21:24:36 +0300

    Smooth zoom

--- Assets/Player/ScrollZoom.cs
+++ Assets/Player/ScrollZoom.cs
@@ -6,9 +6,12 @@ public class ScrollZoom : MonoBehaviour
 {
     public float sensitivity = 0.5f;
     public float maxScroll = Mathf.Infinity;
+    [Range(0f, 1f)]
+    public float smoothing = 0.9f;
 
     private Vector3 initialLocalPosition;
     private float totalScroll = 1f;
+    private float totalScrollSmooth = 1f;
 
     void Start() {
         initialLocalPosition = transform.localPosition;
@@ -17,7 +20,8 @@ public class ScrollZoom : MonoBehaviour
     void Update() {
         totalScroll += -Input.mouseScrollDelta.y * sensitivity;
         totalScroll = Mathf.Clamp(totalScroll, 0f, maxScroll);
-        transform.localPosition = initialLocalPosition * totalScroll;
+        totalScrollSmooth = Mathf.Lerp(totalScrollSmooth, totalScroll, 1f - smoothing);
+        transform.localPosition = initialLocalPosition * totalScrollSmooth;
         if (Input.GetKey(KeyCode.Mouse2)) {
             totalScroll = 1f;
         }
