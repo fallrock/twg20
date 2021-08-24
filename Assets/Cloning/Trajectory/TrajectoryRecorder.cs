1f93c38e523265cf79b21383e5dc7a954d1f3433
Dmitry <deekovalev@gmail.com>
2021-08-25 02:28:12 +0300

    Lifecycle MVP

--- Assets/Cloning/Trajectory/TrajectoryRecorder.cs
+++ Assets/Cloning/Trajectory/TrajectoryRecorder.cs
@@ -7,10 +7,6 @@ public class TrajectoryRecorder : MonoBehaviour
     private Trajectory trajectory;
     private float currentRoundBeginning;
 
-    public void ResetRound() {
-        currentRoundBeginning = Time.time;
-    }
-
     void Start()
     {
         trajectory = GetComponent<Trajectory>();
