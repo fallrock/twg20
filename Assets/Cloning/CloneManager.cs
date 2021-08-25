0866565ecb810ba2de2dde68e82569795c79656d
Dmitry <deekovalev@gmail.com>
2021-08-25 17:47:55 +0300

    Trajectory.cs

--- Assets/Cloning/CloneManager.cs
+++ Assets/Cloning/CloneManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Trajectory = System.Collections.Generic.List<UnityEngine.Vector3>;
 
 public class CloneManager : MonoBehaviour
 {
