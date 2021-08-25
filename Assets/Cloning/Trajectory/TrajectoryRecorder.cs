33deb6aa1ca0a660d3d785cc84e0c9c4c33d6048
Dmitry <deekovalev@gmail.com>
2021-08-25 17:43:45 +0300

    Trajectory refactoring

--- Assets/Cloning/Trajectory/TrajectoryRecorder.cs
+++ Assets/Cloning/Trajectory/TrajectoryRecorder.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Trajectory = System.Collections.Generic.List<UnityEngine.Vector3>;
 
 public class TrajectoryRecorder : MonoBehaviour
 {
-    private Trajectory trajectory;
+    private Trajectory trajectory = new Trajectory();
     private float currentRoundBeginning;
 
-    void Start()
-    {
-        trajectory = GetComponent<Trajectory>();
-        currentRoundBeginning = Time.time;
+    void Start() {
+        Reset();
     }
 
-    void FixedUpdate()
-    {
-        trajectory.Put(new Trajectory.Point(
-                           Time.time - currentRoundBeginning,
-                           transform.position));
+    void FixedUpdate() {
+        trajectory.Add(transform.position);
+    }
+
+    public Trajectory GetTrajectory() {
+        return trajectory;
+    }
+
+    public void Reset() {
+        trajectory.Clear();
+        currentRoundBeginning = Time.time;
     }
 
 }
--- Assets/Player/ExplosiveController.cs
+++ Assets/Player/ExplosiveController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Trajectory = System.Collections.Generic.List<UnityEngine.Vector3>;
 
 public class ExplosiveController : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class ExplosiveController : MonoBehaviour
 
     void Explode() {
         var clones = GameObject.Find("Managers").GetComponent<CloneManager>();
-        clones.Store(GetComponent<Trajectory>());
+        clones.Store(GetComponent<TrajectoryRecorder>().GetTrajectory());
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         lifecycle.RespawnLater();
         Destroy(gameObject);
