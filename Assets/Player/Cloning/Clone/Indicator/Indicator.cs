43e8ba261ecb0955c236345461141306f2ecb052
grillow <spmr757@gmail.com>
2021-08-26 23:29:19 +0300

    explosion indicator 1-x^2

--- Assets/Player/Cloning/Clone/Indicator/Indicator.cs
+++ Assets/Player/Cloning/Clone/Indicator/Indicator.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Indicator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float duration = 1.0f;
+
+    public float rotationFactor = 1.0f;
+    public float scaleFactor = 1.0f;
+
+    public void Initialize(float endTime)
     {
-        
+        this.endTime = Time.time + endTime;
     }
 
-    // Update is called once per frame
+    private float endTime;
+
     void Update()
     {
-        
+        float x = Time.time;
+        float start = endTime - duration;
+
+        float scale = Step(0, x - start) * Step(x - start, duration) * (1 - Mathf.Pow((( x - start) / duration), 2));
+
+        scale *= scaleFactor;
+
+        transform.localScale = new Vector3(scale, transform.localScale.y, scale);
     }
+
+    private float Step(float a, float b)
+    {
+        if (b > a) return 1.0f;
+        return 0.0f;
+    }
+
 }
