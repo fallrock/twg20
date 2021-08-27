3f815fcec2dd186176cbcd95fa263ef2823093d6
grillow <spmr757@gmail.com>
2021-08-27 16:46:32 +0300

    very cool abyss respawning bug

---
+++ Assets/Scenes/Tutorial/Prefabs/Player/KillingAbyss.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingAbyss : MonoBehaviour
+{
+    public float abyssLevel = -10.0f;
+    void Update()
+    {
+        if (transform.position.y < abyssLevel)
+        {
+            gameObject
+                .transform
+                .parent
+                .gameObject
+                .GetComponent<Lifecycle>()
+                .RespawnImmediately();
+        }
+    }
+}
